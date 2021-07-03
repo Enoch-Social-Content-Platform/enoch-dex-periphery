06b18a69506a5821a4ffdea76387f9882d4f7b35
sareen_01 <sanya16sareen@gmail.com>
2021-07-03 23:45:11 +0530

    migrate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,7 +6,7 @@ module.exports = async function (deployer, network) {
   const FACTORY_ADDRESS = '0x7D06f5dA2BdE23203aeF3d20C58FD859018e4E3E';
   
   if(network === 'mainnet'){
-      weth = await WETH.at('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
+      weth = await WETH.at('0xB1BAB8754079ed93F4DD9E73aaaCC64fB921bCF2');
   }
   else{
       await deployer.deploy(WETH);
