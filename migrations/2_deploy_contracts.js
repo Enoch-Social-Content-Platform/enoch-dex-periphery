effbc5a2c627edfda1b12c63c9e483d70a18925f
sareen_01 <sanya16sareen@gmail.com>
2021-07-07 10:57:46 +0530

    deploy factory_address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const WETH = artifacts.require("WETH.sol")
 
 module.exports = async function (deployer, network) {
   let weth;
-  const FACTORY_ADDRESS = '0x7D06f5dA2BdE23203aeF3d20C58FD859018e4E3E';
+  const FACTORY_ADDRESS = '0x2a055D62690f94128C9532AB909ac6261c41304a';
   
   if(network === 'mainnet'){
       weth = await WETH.at('0xB1BAB8754079ed93F4DD9E73aaaCC64fB921bCF2');
