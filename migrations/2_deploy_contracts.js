2b213c25724fec5cd91207f06699023e5c1ca142
sareen_01 <sanya16sareen@gmail.com>
2021-07-07 12:48:57 +0530

    adding Liquidity

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -15,5 +15,27 @@ module.exports = async function (deployer, network) {
 
   await deployer.deploy(Router, FACTORY_ADDRESS, weth.address);
 
-  
+  const ROUTER = await Router.deployed();
+  // ROUTER_ADDRESS = ROUTER.address;
+
+  await ROUTER.addLiquidity(
+    "0xC9aa5Ce92DCE6FD57706689136E1a4d049516553",
+    "0x93AC3A8309BC7775420FAB26bE874a3a5ad4C488",
+    "10",
+    "10",
+    "1",
+    "1",
+    "0xC9aa5Ce92DCE6FD57706689136E1a4d049516553",
+   Math.floor(Date.now()/100) +60*5
+  ) 
+
+ 
+
 };
+
+// await deployer.deploy(Token1);
+//       await deployer.deploy(Token2);
+//       const token1 = await Token1.deployed();
+//       const token2 = await Token2.deployed();
+//       token1Address = token1.address;
+//       token2Address = token2.address;
