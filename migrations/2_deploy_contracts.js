d5d3be8bbc3238fe63a3b58538d3fb3ed0ec3f14
sareen_01 <sanya16sareen@gmail.com>
2021-07-09 15:13:54 +0530

    Using Token1 and Token2 approve methods for Liquidity

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,15 @@
 const Router = artifacts.require("UniswapV2Router02.sol");
 const WETH = artifacts.require("WETH.sol")
 
+const { abi: Token1_ABI } = require("../build/contracts/Token1.json");
+const {abi: Token2_ABI} = require("../build/contracts/Token2.json");
+
 module.exports = async function (deployer, network) {
   let weth;
-  const FACTORY_ADDRESS = '0xFa797BC07347ada632cFa244389f717F09ea4de4';
+  const FACTORY_ADDRESS = '0x361a6519eC6A42F256F43A4fD50fa7fA79EB9bD1';// getting from core contract
+
+  const token1 = new web3.eth.Contract(Token1_ABI);
+  const token2 = new web3.eth.Contract(Token2_ABI);
   
   if(network === 'mainnet'){
       weth = await WETH.at('0xB1BAB8754079ed93F4DD9E73aaaCC64fB921bCF2');
@@ -15,21 +21,20 @@ module.exports = async function (deployer, network) {
 
   await deployer.deploy(Router, FACTORY_ADDRESS, weth.address);
   const ROUTER = await Router.deployed();
-  // ROUTER_ADDRESS = ROUTER.address;
- 
-  // await token1.approve("0x4c13569256E3009DB60B4D4A512E9310838AB244",10)
-  // await token2.approve("0x4c13569256E3009DB60B4D4A512E9310838AB244",10)
+  
+  await token1.methods.approve(ROUTER.address, 10);
+  await token2.methods.approve(ROUTER.address, 10);
 
-  // await ROUTER.addLiquidity(
-  //   "0x0bF6AC75E596c45F44bad4F9231C0F53338f3fD3",
-  //   "0x2997f3AA7Ab93faAd26dF311265A1546a83E2e5A",
-  //   10,
-  //   10,
-  //   1,
-  //   1,
-  //   "0x6CaF44C89Af182202398fe924588C37B1E604C79",
-  //  Math.floor(Date.now()/100) + 60 * 5
-  // ) 
+  await ROUTER.addLiquidity(
+    "0x944d5f4c3CF887D778225cE9fDE20de7Dea0319A", //token1 address
+    "0xd4391ec0b5EB0dCf9b35C03Bc19a97018FAF8A91", //token2 address
+    10,
+    10,
+    1,
+    1,
+    "0x6CaF44C89Af182202398fe924588C37B1E604C79", //account 1
+   Math.floor(Date.now()/100) + 60 * 10
+  ) 
 
 //  await ROUTER.removeLiquidity(
 //   "0xc321B29F4D346E82B665063d34591466Ebf2439B",
