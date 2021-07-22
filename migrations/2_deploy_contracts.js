def192261945c5add194e28c01632d82a9cbf236
sareen_01 <sanya16sareen@gmail.com>
2021-07-22 12:53:46 +0530

    Transfer Token1 from one address to another

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,12 +11,12 @@ const {abi: EnochV2ERC20_ABI} = require("../build/contracts/EnochV2ERC20.json");
 
 module.exports = async function (deployer, network) {
   let weth;
-  const FACTORY_ADDRESS = '0xfAa5Bd59B7Be0b06705D8EaED0636288279860b4';// getting from core contract
-  token1Address = "0x05eA50dc2C0389117A067D393e0395ACc32c53b6";
-  token2Address = "0xc29135482daEb5441520BaC6e8c97bFf194b24eE";
+  const FACTORY_ADDRESS = '0xCfEB869F69431e42cdB54A4F4f105C19C080A601';// getting from core contract
+  token1Address = "0xC89Ce4735882C9F0f0FE26686c53074E09B0D550";
+  token2Address = "0xD833215cBcc3f914bD1C9ece3EE7BF8B14f841bb";
   account_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1";
   // EnochV2ERC20_address = "0xC5aFE31AE505594B190AC71EA689B58139d1C354";
-  pairAddress  = "0x9De65e7B2588Ce00DBfB8045d35330355f285EB3";
+  pairAddress  = "0xa13a25a0B3f75011429Ae81552a65FCCDF78ce65";
   
   const token1 = new web3.eth.Contract(Token1_ABI, token1Address);
   const token2 = new web3.eth.Contract(Token2_ABI, token2Address);
@@ -49,38 +49,40 @@ module.exports = async function (deployer, network) {
   let a2 = await token2.methods.allowance(account_address, ROUTER.address).call()
 
 
-//  console.log(await ROUTER.addLiquidity(
-//   token1Address, 
-//   token2Address, 
-//   100000,
-//   100000,
-//   100,
-//   100,
-//   account_address, 
-//   Math.floor(Date.now()/1000) + (1*60*60),
-//   {from:account_address}
-
-// ))
-
-console.log(await route.methods.addLiquidity(
+ console.log(await ROUTER.addLiquidity(
   token1Address, 
   token2Address, 
-  10000,
-  10000,
+  100000,
+  100000,
   100,
   100,
   account_address, 
   Math.floor(Date.now()/1000) + (1*60*60),
+  {from:account_address}
 
-).call({from:account_address}))
+))
 
-console.log((await route.methods.getAmountsOut(100000, [token1Address, token2Address]).call()))
+// console.log(await route.methods.addLiquidity(
+//   token1Address, 
+//   token2Address, 
+//   10000,
+//   10000,
+//   100,
+//   100,
+//   account_address, 
+//   Math.floor(Date.now()/1000) + (1*60*60),
+
+// ).call({from:account_address}))
+
+// console.log((await route.methods.getAmountsOut(100000, [token1Address, token2Address]).call()))
 
   //SWAP
   // Transfer token 1 to token 2
-  // Transfer token 1 to address[1]
-  
+  // Transfer token 1 to address[1] 
   // msg.sender should have already given the router an allowance of at least amountIn on the input token.
+  account_address2 = "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0";
+  await token1.methods.transfer(account_address2, 1000).send({from:account_address});
+  console.log("Balance of Token1 in 2nd account",await token1.methods.balanceOf(account_address2).call())
 
 
 
